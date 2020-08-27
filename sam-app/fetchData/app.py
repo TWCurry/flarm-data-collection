b65afaf1cbf142895a161e650a34235480c837fd
TWCurry <thomas.w.curry@accenture.com>
2020-08-27 20:48:17 +0100

    Added ability to fetch data from OGN

--- sam-app/fetchData/app.py
+++ sam-app/fetchData/app.py
@@ -1,4 +1,5 @@
-import os, boto3
+import os, boto3, requests
+
 def lambda_handler(event, context):
     # Environment variables
     try:
@@ -12,8 +13,48 @@ def lambda_handler(event, context):
             "statusCode": 500,
             "body": f"Invalid environment variables - {e}"
         }
-    # https://live.glidernet.org/lxml.php?a=0&b=59.355596110016315&c=49.61070993807422&d=3.6474609375000004&e=-13.710937500000002&z=1
+    
+    # Fetch data
+    try:
+        xmlData = fetchData(loMax, loMin, laMax, laMin)
+    except Exception as e:
+        print(f"Could not fetch XML data - {e}")
+        return{
+            "statusCode": 500,
+            "body": f"Error fetching data from OGN - {e}"
+        }
+    
+    # Convert to sorta json document to write to DDB
+    try:
+        convertXml2Json(xmlData)
+    except Exception as e:
+        print(f"Could not convert XML to ddb json - {e}")
+        return{
+            "statusCode": 500,
+            "body": f"Error converting data from XML - {e}"
+        }
+
     return {
         "statusCode": 200,
         "body": "Function executed successfully"
     }
+
+def fetchData(loMax, loMin, laMax, laMin):
+    print("Fetching XML data...")
+    params = {
+        "a": 0,
+        "b": laMax,
+        "c": laMin,
+        "d": loMax,
+        "e": loMin,
+        "z": 1
+    }
+    r = requests.get("https://live.glidernet.org/lxml.php", params=params)
+    if r.status_code < 200 or r.status_code > 299:
+        raise Exception(f"({r.status_code}) {r.text}")
+    print("Successfully fetched XML data.")
+    return r.text
+
+def convertXml2Json(xmlData):
+    for line in xmlData.splitlines():
+        print(line)
