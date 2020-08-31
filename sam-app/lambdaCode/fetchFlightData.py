3c92096fd607e8c78d29f3797546a28477fe7a7f
TWCurry <thomas.w.curry@accenture.com>
2020-08-31 21:25:45 +0100

    Added code for flight data function (missed updating gitignore in last commit)

---
+++ sam-app/lambdaCode/fetchFlightData.py
@@ -0,0 +1,61 @@
+import os, boto3, sys
+from boto3 import resource
+from boto3.dynamodb.conditions import Key
+
+def lambda_handler(event, context):
+    #Query Parameters
+    try:
+        aircraftId = event["queryStringParameters"]["trigraph"]
+    except Exception as e:
+        print(f"Invalid parameters - {e}")
+        return {
+            "statusCode": 400,
+            "body": f"Invalid parameters - {e}"
+        }
+    # Environment variables
+    try:
+        tableName = os.environ["tableName"]
+    except Exception as e:
+        print(f"Invalid environment variables - {e}")
+        return {
+            "statusCode": 500,
+            "body": f"Invalid environment variables - {e}"
+        }
+    try:
+        flightData = fetchData(tableName, aircraftId)
+    except Exception as e:
+        print(f"Could not read data from DynamoDB - {e}")
+        return {
+            "statusCode": 500,
+            "body": f"Could not read data from DynamoDB - {e}"
+        }
+    
+    return{
+        "statusCode": 200,
+        "body": {
+            "Items": flightData
+        }
+    }
+
+def fetchData(tableName, aircraftId):
+    client = boto3.client("dynamodb")
+    items = []
+    response = client.query(
+        ExpressionAttributeValues={
+            ':v1': {
+                'S': aircraftId,
+            },
+        },
+        KeyConditionExpression='trigraph = :v1',
+        TableName=tableName,
+        IndexName="trigraph-index",
+        Select="ALL_ATTRIBUTES",
+
+    )
+    for item in response["Items"]:
+        newItem = {}
+        for key, value in item.items():
+            if key != "ttl":
+                newItem[key] = value["S"]
+        items.append(newItem)
+    return items
